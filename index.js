d612ab4bee86823f5c11b259c3ca4e6b57a9d9ec
sudiptacc <sudiptacc@users.noreply.github.com>
2022-03-01 22:56:47 -0500

    Add new logo and fade in animation, Fix menu bar collapse

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.onscroll = function(){navbarChange()}
 
 function navbarChange() {
     var navbar = document.getElementById("navbar")
-    if (window.scrollY > 300) {
+    if (window.scrollY > 200) {
         navbar.classList.add("navbar-filled")
         navbar.classList.remove("navbar-transparent")
     } else {
