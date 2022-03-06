a7b1f784df54a3912bdfbedd31d799259094a473
sudiptacc <sudiptacc@users.noreply.github.com>
2022-03-05 20:05:25 -0500

    Add team members, buttons, formatting, comments

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+/* Scroll animation constants */
 const FRAMES = 9
 const SCHEDULE = document.getElementById("schedule-section")
-const SCHEDULE_IMAGE = document.getElementById("TESTING-ANIMATION")
+const SCHEDULE_IMAGE = document.getElementById("scroll-animation")
 const START_PERCENTAGE = 40
+const STEP = START_PERCENTAGE / FRAMES
+
+const NAVBAR = document.getElementById("navbar")
+
 var animationChoice = 0
 
+/*
+IntersecionObserver checks whether an element with the "on-scroll-aniamte" class
+has appeared on the page. Once it has, a fadeInLeft or fadeInRight animation is
+applied. Adding the class ensures that the animation only happens once.
+*/
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -25,6 +35,7 @@ document.querySelectorAll(".on-scroll-animate").forEach(entry => {
     }
 })
 
+/* The navbar and schedule are animated based on scrolling */
 window.onscroll = function () { onScroll() }
 
 function onScroll() {
@@ -32,8 +43,8 @@ function onScroll() {
     scheduleAnimate()
 }
 
+/* Make the navbar opaque if the page is scrolled beyond the landing section */
 function navbarChange() {
-    var navbar = document.getElementById("navbar")
     if (window.scrollY > 200) {
         navbar.classList.add("navbar-filled")
         navbar.classList.remove("navbar-transparent")
@@ -46,21 +57,26 @@ function navbarChange() {
 function scheduleAnimate() {
     var rect = SCHEDULE.getBoundingClientRect()
     var distanceFromTop = rect.top
+    /* percentageScroll indicates how close the schedule element is to the top of the viewport */
     var percentageScroll = (distanceFromTop / window.innerHeight) * 100
-    var step = START_PERCENTAGE / FRAMES
 
+    /* Check if the element is actually on the page */
     if (percentageScroll < 0 || percentageScroll > 100) {
         return
     } else {
+        /* 
+        If the element is at around START_PERCENTAGE or lower, then its current percentageScroll
+        determines the animation's frame. This also lets the animation to be reversed 
+        */
         if (percentageScroll <= START_PERCENTAGE) {
-            var currentFrame = getFrameFromPercentage(percentageScroll, step)
+            /* Gets the closest frame based on the STEP interval, depending on the current percentageScroll */
+            var currentFrame = Math.floor(FRAMES - (percentageScroll / STEP))
+            if (currentFrame > 8) {
+                currentFrame = 8
+            }
             SCHEDULE_IMAGE.src = `resources/frames/${currentFrame}.png`
         }
     }
-}
-
-function getFrameFromPercentage(percentage, step) {
-    return Math.floor(FRAMES - (percentage / step))
 }
 
 
